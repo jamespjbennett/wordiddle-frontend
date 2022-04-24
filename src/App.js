bc7f0c62ff6f2640576b3dc170264c39cf1e14c4
jamespjbennett <jamespjbennett@hotmail.com>
2022-04-24 10:18:08 +0100

    Sort out some UI stuff

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
-import React, { useState, useEffect } from 'react';
+import React, { useState, useEffect,useRef } from 'react';
 
 
 const App = () => {
@@ -11,12 +11,36 @@ const WordGrid = () => {
   const [chosenWord, setChosenWord] = useState(
     ["", "", "", "", ""]
   );
+  const inputRef = useRef({});
 
   function updateWord(event) {
     let existingCurrentWord = chosenWord;
     existingCurrentWord[Number(event.target.name)] = event.target.value
     setChosenWord(existingCurrentWord);
-    console.log(chosenWord)
+    console.log(event)
+    if(event.target.parentElement.nextElementSibling){
+      event.target.parentElement.nextElementSibling.firstElementChild.focus()
+    }
+  }
+
+  function submitWord(event){
+    if(event.key == "Enter"){
+      if(validWord(chosenWord)){
+        console.log('ready to submit');
+      }else{
+        console.log('invalid')
+      }
+    }
+  };
+
+  function chosenWordToString(){
+    return chosenWord.join("")
+  }
+
+  function validWord(chosenWord){
+    let correctWordLength = chosenWordToString().length == 5;
+    let onlyAlphaChars = /^[a-zA-Z]+$/.test(chosenWordToString());
+    return correctWordLength && onlyAlphaChars
   }
 
   function InputFields(){
@@ -24,7 +48,15 @@ const WordGrid = () => {
 
     for (var i = 0; i <= 4; i++) {
       fieldsArray.push(
-        <div className="RowBlock"><input key={i} name={i} onChange={updateWord}/></div>
+        <div className="RowBlock">
+          <input
+            key={i}
+            name={i}
+            onChange={updateWord}
+            onKeyPress={submitWord}
+            ref={el => inputRef.current[i] = el}
+          />
+        </div>
       );
     };
     return fieldsArray;
